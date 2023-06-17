320ae1af2ee7e29b4884ef1a676c416b540cbc23
flaviomreis <flaviomreis@gmail.com>
2023-06-17 16:59:05 -0300

    define como página em construção

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
         <div>
             <h1>Controle de Usuários de Curso EaD</h1>
             <p>Faça login para poder controlar os usuários de um curso EaD da sua instituição.</p>
+            <p>PÁGINA EM CONSTRUÇÃO</p>
         </div>
     )
 }
