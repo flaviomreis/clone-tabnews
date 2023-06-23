142835dbde5c0ae7361966f56fad20164e49cb49
flaviomreis <flaviomreis@gmail.com>
2023-06-23 19:11:30 -0300

    adiciona a extensão prettier e scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 function Home() {
-    return (
-        <div>
-            <h1>Controle de Usuários de Curso EaD</h1>
-            <p>Faça login para poder controlar os usuários de um curso EaD da sua instituição.</p>
-            <p>PÁGINA EM CONSTRUÇÃO</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Controle de Usuários de Curso EaD</h1>
+      <p>
+        Faça login para poder controlar os usuários de um curso EaD da sua
+        instituição.
+      </p>
+      <p>PÁGINA EM CONSTRUÇÃO</p>
+    </div>
+  );
 }
 
 export default Home;
